4a6c109c6cac41514261e16f36be1eeda080a5de
MrJacooo <sebastian.storz2003@gmail.com>
2021-09-05 16:02:06 +0200

    Added multiple Languages

--- src/App.js
+++ src/App.js
@@ -8,13 +8,24 @@ function App() {
   const sentences = getSentences()
   const [userInput, setUserInput] = useState("")
   const [currentSentence, setCurrentSentence] = useState({
-    res1: "Bei Andeo zu arbeiten ist eine grossartige ",
-    res2: " !",
-    eng: "Working at Andeo is a great experience!",
-    word: "Erfahrung"
+    trans1: "Bei Andeo zu arbeiten ist eine grossartige ",
+    trans2: " !",
+    word: "Erfahrung",
+    original: "Working at Andeo is a great experience!",
+    sentenceId: 0
   })
   const [showResults, setShowResults] = useState(false)
   const [result, setResult] = useState("")
+  const [clickMe, setClickMe] = useState("")
+  const [originalLang, setOriginalLang] = useState(0)
+  const [transLang, setTransLang] = useState(1)
+  const langList = [
+    { display: "English", internal: "eng" },
+    { display: "German", internal: "ger" },
+    { display: "Spanish", internal: "span" },
+    { display: "French", internal: "fr" },
+    { display: "English", internal: "eng" },
+  ]
 
 
 
@@ -30,15 +41,77 @@ function App() {
     }
     //Get the two curated Start and End Strings
     let strings = input.split(word)
-    let outputObj = { eng: inputObj.eng, res1: strings[0], res2: strings[1], word: word }
+    let outputObj = { original: inputObj.eng, trans1: strings[0], trans2: strings[1], word: word }
     setCurrentSentence(outputObj)
     setShowResults(false)
     setUserInput("")
   }
 
   function checkResult() {
-    setResult(currentSentence.word === userInput ? "Richtig!" : "Falsch.")
+    setResult(currentSentence.word === userInput ? "Richtig!" : "Falsch - \"" + currentSentence.word + "\" wäre richtig gewesen")
     setShowResults(true)
+  }
+
+  //Reloads the Source Sentence according to its Language
+  function reloadOriginal(val) {
+    let sentence = ""
+    let id = val === 1000 ? currentSentence.sentenceId : val
+    let add = val === 1000 ? 1 : 0
+    //Selecting the Sentence in the right Language, it is a bit complicated because of how React handles Re-Renders
+    if (langList[originalLang + add].internal === "eng") sentence = sentences[id].eng;
+    if (langList[originalLang + add].internal === "ger") sentence = sentences[id].ger;
+    if (langList[originalLang + add].internal === "span") sentence = sentences[id].span;
+    if (langList[originalLang + add].internal === "fr") sentence = sentences[id].fr;
+    return { original: sentence }
+
+  }
+  //Reloads the Translate Sentence according to its Language
+  function reloadTrans(val) {
+    let sentence = ""
+    let id = val === 1000 ? currentSentence.sentenceId : val
+    let add = val === 1000 ? 1 : 0
+    //Selecting the Sentence in the right Language, it is a bit complicated because of how React handles Re-Renders
+    if (langList[transLang + add].internal === "eng") sentence = sentences[id].eng;
+    if (langList[transLang + add].internal === "ger") sentence = sentences[id].ger;
+    if (langList[transLang + add].internal === "span") sentence = sentences[id].span;
+    if (langList[transLang + add].internal === "fr") sentence = sentences[id].fr;
+    console.log(sentence)
+    //Get it as an Array to extract a random word
+    let inputArr = sentence.split(" ")
+    let word = inputArr[Math.floor(Math.random() * inputArr.length)]
+    //Check if word has a special Char at the end and then remove it so it remains in the String when we later split it
+    if (["!", ",", "."].some(v => word.includes(v))) {
+      word = word.slice(0, -1)
+    }
+    //Get the two curated Start and End Strings
+    let strings = sentence.split(word)
+    return { trans1: strings[0], trans2: strings[1], word: word, sentenceId: id }
+
+  }
+
+  function changeLang(src) {
+    setClickMe("none")
+    setShowResults(false)
+    setUserInput("")
+    //Only exists because react rendering sucks
+    let nextSentenceId = 1000
+    let obj1 = {}
+    let obj2 = {}
+    if (src.includes("Next")) {
+      nextSentenceId = Math.floor(Math.random() * sentences.length)
+      obj1 = reloadOriginal(nextSentenceId)
+      obj2 = reloadTrans(nextSentenceId)
+    }
+    //Reloads the Sentences in the right Language
+    if (src.includes("Original")) {
+      setOriginalLang((originalLang + 1) % 4)
+      obj1 = reloadOriginal(nextSentenceId)
+    }
+    if (src.includes("Trans")) {
+      setTransLang((transLang + 1) % 4)
+      obj2 = reloadTrans(nextSentenceId)
+    }
+    setCurrentSentence({ ...currentSentence, ...obj1, ...obj2 })
   }
 
 
@@ -49,16 +122,16 @@ function App() {
       </header >
       <section className="round">
         <div class="content shadow round">
-          <h3>Original - English</h3>
-          <p>{currentSentence.eng}</p>
+          <h3>Original - <button className="invisButton" onClick={() => changeLang("Original")}>{langList[originalLang].display} <b style={{ display: `${clickMe}` }}>&lt;click</b> </button></h3>
+          <p>{currentSentence.original}</p>
         </div>
         <div class="content shadow round">
-          <h3>Translate - Deutsch</h3>
-          <p>{currentSentence.res1}<input value={userInput} onChange={e => setUserInput(e.target.value)} />{currentSentence.res2}</p>
+          <h3>Translate - <button className="invisButton" onClick={() => changeLang("Trans")}>{langList[transLang].display} <b style={{ display: `${clickMe}` }}>&lt;click</b> </button></h3>
+          <p>{currentSentence.trans1}<input value={userInput} onChange={e => setUserInput(e.target.value)} />{currentSentence.trans2}</p>
         </div>
         <div className="shadow-inv-big round content result">
           <button className="round green shadow" onClick={checkResult}>Check</button>
-          <button className="round blue shadow" onClick={prepSentences}>Next</button>
+          <button className="round blue shadow" onClick={() => changeLang("Next")}>Next</button>
           {showResults && <div className="shadow round">
             <div>Ergebnis: {result}</div>
           </div>}
--- src/controller.js
+++ src/controller.js
@@ -1,17 +1,41 @@
 export function getSentences() {
+    //Bei bedarf könnte man hier in der Zukunft ein Backend einfügen, deshalb auch die Struktur.
     const sentences = [
         {
             ger: "Bei Andeo zu arbeiten ist eine grossartige Erfahrung!",
-            eng: "Working at Andeo is a great experience!"
+            eng: "Working at Andeo is a great experience!",
+            span: "Trabajar en Andeo es una gran experiencia.",
+            fr: "Travailler chez Andeo est une expérience formidable!"
         }, {
             ger: "Im Haus schläft Herr Müller.",
-            eng: "Mr Müller is sleeping in the House."
+            eng: "Mr Müller is sleeping in the House.",
+            span: "El Sr. Müller está durmiendo en la casa.",
+            fr: "M. Müller dort dans la maison."
         }, {
             ger: "Das Wetter Heute ist schön Sonnig, nicht wie Gestern.",
-            eng: "The weather today is sunny unlike yesterday."
+            eng: "The weather today is sunny unlike yesterday.",
+            span: "El tiempo hoy es agradable y soleado, no como ayer.",
+            fr: "Le temps aujourd'hui est beau et ensoleillé, pas comme hier."
         }, {
-            ger: "Der 150Fr Digitec Gutschein wäre nice, das Bewerbungsgespräch aber noch besser.",
-            eng: "The 150Fr Digitec coupon would be nice, the job interview would be even nicer."
+            ger: "Der 150Fr Digitec Gutschein wäre schön, das Bewerbungsgespräch aber noch besser.",
+            eng: "The 150Fr Digitec coupon would be nice, the job interview would be even nicer.",
+            span: "El bono de 150Fr de Digitec estaría bien, pero la entrevista sería aún mejor.",
+            fr: "Le bon Digitec de 150 Fr serait bien, mais l'entretien serait encore mieux."
+        }, {
+            ger: "Der Hase wird vom Wolf gejagt.",
+            eng: "The bunny is being hunted by the Wolf.",
+            span: "La liebre es cazada por el lobo.",
+            fr: "Le lièvre est chassé par le loup."
+        }, {
+            ger: "Hunde gelten als der beste Freund des Menschen.",
+            eng: "Dogs are known to be the best friends of humans.",
+            span: "Los perros son considerados el mejor amigo del hombre.",
+            fr: "Les chiens sont considérés comme le meilleur ami de l'homme."
+        }, {
+            ger: "Bildung ist die mächtigste Waffe um die Welt zu ändern. Nelson Mandela",
+            eng: "Education is the most powerful weapon which you can use to change the world. Nelson Mandela",
+            span: "La educación es el arma más poderosa para cambiar el mundo. Nelson Mandela",
+            fr: "L'éducation est l'arme la plus puissante pour changer le monde. Nelson Mandela"
         }
     ]
     return sentences
